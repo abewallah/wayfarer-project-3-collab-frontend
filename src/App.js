4718aa3caf3d95776cece3205c43dcd3e385e161
abewallah <ibrahimabdull93@gmail.com>
2021-03-01 21:20:46 -0800

    ignore

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import Navbar from './components/Navbar';
 import React from 'react';
+import Navbar from './components/navbar/Navbar';
 import routes from './config/routes';
 import Footer from './components/Footer';
 
