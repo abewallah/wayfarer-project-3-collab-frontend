9159fb7775078d241a9142bea76a19f86ff8b39f
abewallah <ibrahimabdull93@gmail.com>
2021-03-11 12:25:40 -0800

    depoloyment

--- src/config/routes.js
+++ src/config/routes.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
 import WayfarerContainer from '../containers/WayfarerContainer';
-// import LoginPage from '../pages/LoginPage';
-import SignupPage from '../pages/SignupPage';
+
 import ShowPostPage from '../pages/ShowPostPage'
 import EditPost from '../pages/EditPost'
 import AboutPage from '../pages/AboutPage';
@@ -13,8 +12,6 @@ export default (
   <Switch>
     <Route exact path='/' component={ HomePage } />
     <Route path='/wayfarer' component={ WayfarerContainer } />
-    {/* <Route path='/login' component={ LoginPage } /> */}
-    <Route path='/signup' component={ SignupPage } />
     <Route path='/show-post/:id' component={ ShowPostPage } />
     <Route path='/edit-post/:id' component={EditPost} />
     <Route path='/about' component={AboutPage} />
